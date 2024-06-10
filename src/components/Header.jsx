7eebbd3be90ec0bac2803f3fd16e55e5d346629b
luciazabaletag <luciazabaletag@gmail.com>
2024-06-10 16:16:44 +0200

    agregar rutas de enlaces

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
 import Navbar from "./Navbar"
 
-
 const Header = () => {
+
+  const handleCopy = async () => {
+    try {
+      let content = "luciazabaletag@gmail.com";
+      const text = document.getElementById("copy-text")
+      text.classList.remove("info__icons__tooltip-copyOff")
+      text.classList.add("info__icons__tooltip-copyOn")
+      setTimeout(() => {
+        text.classList.add("info__icons__tooltip-copyOff")
+      }, 3000);
+      await navigator.clipboard.writeText(content);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <header id="header" className="header">
       <img className="header__circlesleft" src="src/assets/img/puntos.svg" alt="img puntos" />
@@ -14,7 +29,7 @@ const Header = () => {
                 <p className="info__text">¡Hola!,</p>
                 <p className="info__text">Soy Lucia Zabaleta</p>
                 <p className="info__text">Front-End Developer</p>
-                <button className="info__btn" type="button"><span>Contáctame</span></button>
+                <a className="info__btn" href="#contactid">Contáctame</a>
               </div>
 
               <div>
@@ -22,9 +37,13 @@ const Header = () => {
               </div>
 
               <div className="info__icons">
-                <a className="info__icons__a" href="#" title="Github"><img className="info__icons__img" src="src/assets/img/github.svg" alt="github icon" /></a>
-                <a className="info__icons__a" href="#" title="LinkedIn"><img className="info__icons__img" src="src/assets/img/linkedin.svg" alt="linkedin icon" /></a>
-                <a className="info__icons__a" href="#" title="Email"><img className="info__icons__img" src="src/assets/img/email.svg" alt="email icon" /></a>
+                <a className="info__icons__a" href="https://github.com/luciazabaletag" target="_blank" rel="noreferrer" title="Github"><img className="info__icons__img" src="src/assets/img/github.svg" alt="github icon" /></a>
+                <a className="info__icons__a" href="https://www.linkedin.com/in/luciazabaletagirod" target="_blank" rel="noreferrer" title="LinkedIn"><img className="info__icons__img" src="src/assets/img/linkedin.svg" alt="linkedin icon" /></a>
+                <a className="info__icons__a info__icons__a-tooltip" onClick={handleCopy}>
+                  <img className="info__icons__img" src="src/assets/img/email.svg" alt="email icon" />
+                  <span id="copy-tooltip" className="info__icons__tooltip">luciazabaletag@gmail.com <span className="info__icons__tooltip-span">Click para copiarlo</span></span>
+                  <span id="copy-text" className="info__icons__tooltip-copyOff">¡Email copiado!</span>
+                </a>
               </div>
 
           </div>
